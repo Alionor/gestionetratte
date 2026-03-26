bc38a2cda6899dbaba08a59ff00285f591076aa4
Alionor <elegrisa32@libero.it>
2026-03-26 10:44:19 +0100

    model implementato

---
+++ src/main/java/it/prova/gestionetratte/model/Airbus.java
@@ -0,0 +1,87 @@
+package it.prova.gestionetratte.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "airbus")
+public class Airbus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "codice")
+    private String codice;
+
+    @Column(name = "descrizione")
+    private String descrizione;
+
+    @Column(name = "datainizioservizio")
+    private LocalDate dataInizioServizio;
+
+    @Column(name = "numeropasseggeri")
+    private Integer numeroPasseggeri;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "airbus")
+    private Set<Tratta> tratte = new HashSet<>();
+
+    public Airbus() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public LocalDate getDataInizioServizio() {
+        return dataInizioServizio;
+    }
+
+    public void setDataInizioServizio(LocalDate dataInizioServizio) {
+        this.dataInizioServizio = dataInizioServizio;
+    }
+
+    public Integer getNumeroPasseggeri() {
+        return numeroPasseggeri;
+    }
+
+    public void setNumeroPasseggeri(Integer numeroPasseggeri) {
+        this.numeroPasseggeri = numeroPasseggeri;
+    }
+
+    public Set<Tratta> getTratte() {
+        return tratte;
+    }
+
+    public void setTratte(Set<Tratta> tratte) {
+        this.tratte = tratte;
+    }
+}
+
+
+
+
